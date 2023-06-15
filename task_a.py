caa2678291cd336123510b59308df182b2e5f7e8
9P1xel <re9pixel@gmail.com>
2023-06-15 14:32:00 +0300

    task a(2)

--- task_a.py
+++ task_a.py
@@ -4,14 +4,10 @@
 все числа, которые делятся на 3 и не делятся на 5'''
 
 lst = [5,4,3,2,1,13,45,67,87,89,90,657,15,851,32]
-min = lst[0]
-max = lst[0]
+min = min(lst)
+max = max(lst)
 digits = []
 for i in lst:
-  if min >= i:
-    min = i
-  elif max <= i:
-    max = i
   if i % 3 == 0 and i % 5 != 0:
     digits.append(i)
 print("max = ", max)
