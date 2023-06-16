c3a1a435db87fd5ba852300b9de28c0a0a2eef40
9P1xel <re9pixel@gmail.com>
2023-06-16 17:48:14 +0300

    zip list

---
+++ homework2.py
@@ -0,0 +1,13 @@
+'''НА ВХОД ПОДАЮТСЯ ДВА СПИСКА: АВТОМОБИЛИ И ИМЕНА
+НЕОБХОДИМО ПОДОБРАТЬ АВТОМОБИЛЬ ВЛАДЕЛЬЦУ НА ОСНОВАНИИ АЛФАВИТНОГО ПОРЯДКА
+В СЛУЧАЕ, ЕСЛИ СПИСКИ НЕ ОДИНАКОВЫ ПО ДЛИНЕ, ВЫДАТЬ СООБЩЕНИЕ О ТОМ,
+ЧТО КОМУ-ТО НЕ ДОСТАНЕТСЯ АВТОМОБИЛЬ'''
+
+autos = [a for a in input().split()]
+names = [n for n in input().split()]
+names.sort()
+autos.sort()
+match = list(zip(names,autos))
+print(match)
+if len(names)>len(autos):
+    print("sold out")
