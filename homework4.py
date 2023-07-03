1b0630a9cfbde74d54d270c5fec63b9988839c45
9P1xel <re9pixel@gmail.com>
2023-07-04 01:10:55 +0300

    Transformer

--- homework4.py
+++ homework4.py
@@ -4,11 +4,13 @@
 и обратный ему decimal_to_roman.'''
 
 class Transformer:
-    
+    # converts roman numerals to decimal
+    # and decimal to roman
     def __init__(self, tallies = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}):
         self.tallies = tallies
 
     def roman_to_decimal(self, roman_number):
+        # converts roman numerals to decimal
         sum = 0
         for i in range(len(roman_number) - 1):
             left = roman_number[i]
@@ -21,6 +23,7 @@ class Transformer:
         return sum
 
     def decimal_to_roman(self, decimal_number):
+        # converts numerals decimal to roman
         result = ""
         for key, value in list(sorted(self.tallies.items(), key=lambda item: item[1], reverse=True)):
             while decimal_number >= value:
